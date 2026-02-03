3167a004e8b3531214800d5befee4fc5523fee0a
ctgold <ctgold@users.noreply.github.com>
2026-02-03 19:57:26 +0800

    feat(admin): add new super admin user ID
    - Add dfbbf71c-0a7c-43fb-bab0-d21f12b78b47 to admin whitelist
    - User now has full admin access to all admin routes
    - Admin guards will recognize this user as super admin

--- src/config/admin.ts
+++ src/config/admin.ts
@@ -1,5 +1,6 @@
 export const ADMIN_USER_IDS = [
-    "518694f6-bb50-4724-b4a5-77ad30152e0e"
+    "518694f6-bb50-4724-b4a5-77ad30152e0e",
+    "dfbbf71c-0a7c-43fb-bab0-d21f12b78b47"
 ] as const;
 
 export function isAdminUserId(id?: string | null) {
